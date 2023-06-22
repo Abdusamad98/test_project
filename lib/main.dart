976eeb8f84afb5360058d13f2a6d2691ec382b8e
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-06-22 12:37:06 +0500

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,14 @@ void main() {
     debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
+        title: const Text(
+          "Spedah",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: 24,
+          ),
+        ),
         systemOverlayStyle: const SystemUiOverlayStyle(
           statusBarColor: Color(0xFF1F5460),
         ),
@@ -13,6 +21,11 @@ void main() {
         backgroundColor: const Color(0xFF1F5460),
       ),
       backgroundColor: const Color(0xFF1F5460),
+      body: Column(
+        children: [
+
+        ],
+      ),
     ),
   ));
 }
