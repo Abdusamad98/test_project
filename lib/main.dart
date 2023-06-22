8c74745226cd1aea4fb6c866b4103cb904081538
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-06-22 12:47:23 +0500

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,82 @@ void main() {
         backgroundColor: const Color(0xFF1F5460),
       ),
       backgroundColor: const Color(0xFF1F5460),
-      body: Column(
-        children: [
-
-        ],
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Image.asset(
+              "assets/images/bicycle.png",
+              height: 300,
+            ),
+            const SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  "Let's\nget started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 28),
+            const SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  "Everything start from here",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            Container(
+              height: 50,
+              margin: const EdgeInsets.symmetric(horizontal: 28),
+              decoration: BoxDecoration(
+                  color: const Color(0xFFFFCA42),
+                  borderRadius: BorderRadius.circular(100)),
+              child: const Center(
+                child: Text(
+                  "Log in",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 50,
+              margin: const EdgeInsets.symmetric(horizontal: 28),
+              decoration: BoxDecoration(
+                  color: const Color(0xFFD5E7D4),
+                  borderRadius: BorderRadius.circular(100)),
+              child: const Center(
+                child: Text(
+                  "Sign up",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     ),
   ));
