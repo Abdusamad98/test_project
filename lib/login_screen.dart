347eb003cfb1a7c5dbe7380414ef2ba6663b8046
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-07-04 12:16:33 +0500

    some changes

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -7,19 +7,179 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
-        systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: Colors.white),
+        systemOverlayStyle:
+            const SystemUiOverlayStyle(statusBarColor: Colors.white),
         backgroundColor: Colors.white,
-        title: Text("Login Screen"),
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: Icon(
+          icon: const Icon(
             Icons.arrow_back,
             color: Colors.black,
           ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(28),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 8),
+            const Text(
+              "Welcome back",
+              style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xFF1F5460)),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              "Enter your credential to continue",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xFF1F5460)),
+            ),
+            const SizedBox(height: 36),
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.person),
+                hintText: "Email or username",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Color(0xFF1F5460).withOpacity(0.2),
+                  ),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.lock),
+                suffixIcon: const Icon(Icons.remove_red_eye),
+                hintText: "Password",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Color(0xFF1F5460).withOpacity(0.2),
+                  ),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "Forgot password?",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF1F5460)),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: const Color(0xFFFFCA42),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: const Center(
+                child: Text(
+                  "LogIn",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/apple.png",
+                    width: 23,
+                    height: 23,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    "LogIn using Apple",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color(0xFFF0F5F2),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/google.png",
+                    width: 23,
+                    height: 23,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    "LogIn using Google",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xFF10405A),
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Expanded(child: SizedBox()),
+            const Center(
+              child: Text(
+                "Don’t have account? Sign up",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Color(0xFF1F5460),
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
