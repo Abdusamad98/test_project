bfb307b3961e3923bed7fcb4ce9d4e327c21d2d4
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-07-05 11:16:52 +0500

    some changes

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:test_project/ui/contacts_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ContactsScreen(),
+    ),
+  );
+}
---
+++ lib/ui/contacts_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ContactsScreen extends StatefulWidget {
+  const ContactsScreen({super.key});
+
+  @override
+  State<ContactsScreen> createState() => _ContactsScreenState();
+}
+
+class _ContactsScreenState extends State<ContactsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.more_vert,
+              color: Colors.black,
+            ),
+          )
+        ],
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Contacts",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
