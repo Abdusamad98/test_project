f5992bc64f9605f18fac2ffacfcad1b1895e15d0
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-07-17 09:13:26 +0500

    some changes

---
+++ lib/ui/contact_detail_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:test_project/models/contact_model.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class ContactDetailScreen extends StatelessWidget {
+  const ContactDetailScreen({super.key, required this.contactModel});
+
+  final ContactModel contactModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Contact Details",
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.more_vert,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+            children: [
+            const SizedBox(height: 50),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const Expanded(child: SizedBox()),
+            const Icon(
+              Icons.account_circle,
+              size: 100,
+              color: Colors.grey,
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.delete,
+                      color: Colors.black,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.edit,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Text(
+          contactModel.contactName,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 25),
+        Row(
+          children: [
+          Text(
+          contactModel.contactPhone,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const Expanded(child: SizedBox()),
+        GestureDetector(
+            onTap: () async {
+              await launchUrl(
+                  Uri.parse("tel:${contactModel.contactPhone}"));
+            },
+            child: Container(
+            height: 40,
+            width: 40,
+        decoration: const BoxDecoration(
+        shape: BoxShape.circle, color: Colors.green),
+        child: const Icon(
+          Icons.call,
+          size: 16,
+          color: Colors.white,
+        ),
+      ),
+    ),
+    const SizedBox(width: 15),
+    Container(
+    height: 40,
+    width: 40,
+    decoration: const BoxDecoration(
+    shape: BoxShape.circle, color: Colors.orangeAccent),
+    child: const Icon(
+    Icons.message,
+    size: 16,
+    color: Colors.white,
+    ),
+    )
+    ],
+    )
+    ],
+    ),
+    ),
+    );
+    }
+}
--- lib/ui/contacts_screen.dart
+++ lib/ui/contacts_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:test_project/models/contact_model.dart';
 import 'package:test_project/ui/add_contact_screen.dart';
+import 'package:test_project/ui/contact_detail_screen.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ContactsScreen extends StatefulWidget {
@@ -11,12 +13,11 @@ class ContactsScreen extends StatefulWidget {
 }
 
 class _ContactsScreenState extends State<ContactsScreen> {
-
   List<ContactModel> contacts = [
-      ContactModel(
-        contactPhone: "+99890 974 94 62",
-        contactName: "Abdulloh",
-      )
+    ContactModel(
+      contactPhone: "+99890 974 94 62",
+      contactName: "Abdulloh",
+    )
   ];
 
   @override
@@ -47,46 +48,77 @@ class _ContactsScreenState extends State<ContactsScreen> {
           )
         ],
       ),
-      body: ListView(
-        children: [
-          for (int i = 0; i < contacts.length; i++)
-            ListTile(
-              onTap: () {},
-              trailing: IconButton(
-                onPressed: () async {
-                  await launchUrl(Uri.parse("tel:${contacts[i].contactPhone}"));
-                },
-                icon: const Icon(
-                  Icons.call,
-                  color: Colors.green,
-                ),
-              ),
-              leading: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.account_circle,
-                  color: Colors.grey,
-                  size: 40,
-                ),
-              ),
-              title: Text(
-                contacts[i].contactName,
-                style: const TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16),
-              ),
-              subtitle: Text(
-                contacts[i].contactPhone,
-                style: const TextStyle(
-                  color: Color(0xFF8B8B8B),
-                  fontWeight: FontWeight.w500,
-                  fontSize: 14,
-                ),
-              ),
+      body: contacts.isNotEmpty
+          ? ListView(
+              children: [
+                for (int i = 0; i < contacts.length; i++)
+                  ListTile(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return ContactDetailScreen(
+                              contactModel: contacts[i],
+                            );
+                          },
+                        ),
+                      );
+                    },
+                    trailing: IconButton(
+                      onPressed: () async {
+                        await launchUrl(
+                            Uri.parse("tel:${contacts[i].contactPhone}"));
+                      },
+                      icon: const Icon(
+                        Icons.call,
+                        color: Colors.green,
+                      ),
+                    ),
+                    leading: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.account_circle,
+                        color: Colors.grey,
+                        size: 40,
+                      ),
+                    ),
+                    title: Text(
+                      contacts[i].contactName,
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16),
+                    ),
+                    subtitle: Text(
+                      contacts[i].contactPhone,
+                      style: const TextStyle(
+                        color: Color(0xFF8B8B8B),
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                      ),
+                    ),
+                  )
+              ],
             )
-        ],
-      ),
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  "assets/svg/empty.svg",
+                  width: 100,
+                ),
+                const Center(
+                  child: Text(
+                    "You have no contacts yet",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                )
+              ],
+            ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -94,7 +126,7 @@ class _ContactsScreenState extends State<ContactsScreen> {
             MaterialPageRoute(
               builder: (context) {
                 return AddContactScreen(
-                  onNewContact: (newContact){
+                  onNewContact: (newContact) {
                     setState(() {
                       contacts.add(newContact);
                     });
