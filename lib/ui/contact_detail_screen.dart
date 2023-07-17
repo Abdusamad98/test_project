91007e858f6beff2efa00177ddee3cb0ce703412
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-07-17 09:13:54 +0500

    some changes

--- lib/ui/contact_detail_screen.dart
+++ lib/ui/contact_detail_screen.dart
@@ -45,95 +45,95 @@ class ContactDetailScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Column(
-            children: [
-            const SizedBox(height: 50),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            const Expanded(child: SizedBox()),
-            const Icon(
-              Icons.account_circle,
-              size: 100,
-              color: Colors.grey,
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const Expanded(child: SizedBox()),
+                const Icon(
+                  Icons.account_circle,
+                  size: 100,
+                  color: Colors.grey,
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.black,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.edit,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.delete,
-                      color: Colors.black,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.edit,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
+            const SizedBox(height: 20),
+            Text(
+              contactModel.contactName,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
               ),
             ),
+            const SizedBox(height: 25),
+            Row(
+              children: [
+                Text(
+                  contactModel.contactPhone,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const Expanded(child: SizedBox()),
+                GestureDetector(
+                  onTap: () async {
+                    await launchUrl(
+                        Uri.parse("tel:${contactModel.contactPhone}"));
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.green),
+                    child: const Icon(
+                      Icons.call,
+                      size: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 15),
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.orangeAccent),
+                  child: const Icon(
+                    Icons.message,
+                    size: 16,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            )
           ],
         ),
-        const SizedBox(height: 20),
-        Text(
-          contactModel.contactName,
-          style: const TextStyle(
-            color: Colors.black,
-            fontSize: 22,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        const SizedBox(height: 25),
-        Row(
-          children: [
-          Text(
-          contactModel.contactPhone,
-          style: const TextStyle(
-            color: Colors.black,
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        const Expanded(child: SizedBox()),
-        GestureDetector(
-            onTap: () async {
-              await launchUrl(
-                  Uri.parse("tel:${contactModel.contactPhone}"));
-            },
-            child: Container(
-            height: 40,
-            width: 40,
-        decoration: const BoxDecoration(
-        shape: BoxShape.circle, color: Colors.green),
-        child: const Icon(
-          Icons.call,
-          size: 16,
-          color: Colors.white,
-        ),
       ),
-    ),
-    const SizedBox(width: 15),
-    Container(
-    height: 40,
-    width: 40,
-    decoration: const BoxDecoration(
-    shape: BoxShape.circle, color: Colors.orangeAccent),
-    child: const Icon(
-    Icons.message,
-    size: 16,
-    color: Colors.white,
-    ),
-    )
-    ],
-    )
-    ],
-    ),
-    ),
     );
-    }
+  }
 }
