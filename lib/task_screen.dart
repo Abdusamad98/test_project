b79ce4e3d10504f3182757a77f67f5a0fe0bad84
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-08-30 11:23:52 +0500

    some changes

--- lib/task_screen.dart
+++ lib/task_screen.dart
@@ -9,99 +9,135 @@ class TaskScreen extends StatefulWidget {
 }
 
 class _TaskScreenState extends State<TaskScreen> {
-
   List<LinearGradient> gradients = [
     const LinearGradient(
-        begin: Alignment.bottomLeft,
-        end: Alignment.topRight,
-        colors: [
-          Color(0xFF0052D4),
-          Color(0xFF4364F7),
-          Color(0xFF6FB1FC),
-        ]),
-    const LinearGradient(colors: [
-      Color(0xFF4776E6),
-      Color(0xFF8E54E9),
-    ]),
-    const LinearGradient(colors: [
-      Color(0xFFEC008C),
-      Color(0xFFFC6767),
-    ]),
-    const LinearGradient(colors: [
-      Colors.white,
-      Colors.black,
-    ]),
-    const LinearGradient(colors: [
-      Colors.green,
-      Colors.grey,
-    ])
+      colors: [
+        Color(0xFF0052D4),
+        Color(0xFF4364F7),
+        Color(0xFF6FB1FC),
+      ],
+    ),
+    const LinearGradient(
+      colors: [
+        Color(0xFF4776E6),
+        Color(0xFF8E54E9),
+      ],
+    ),
+    const LinearGradient(
+      colors: [
+        Color(0xFFEC008C),
+        Color(0xFFFC6767),
+      ],
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFD1D1D1),
       appBar: AppBar(
-        leading: Image.asset(("assets/images/arrow_back.png")),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.blue,
+          ),
+        ),
         elevation: 0,
         backgroundColor: Colors.white,
         centerTitle: true,
-        title: Text("Discount",
-            style: GoogleFonts.rubik(
-              color: Colors.blue,
-              fontSize: 22,
-              fontWeight: FontWeight.w500,
-            )),
+        title: Text(
+          "Discount",
+          style: GoogleFonts.rubik(
+            color: Colors.blue,
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(16),
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
             child: TextField(
               decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.symmetric(
-                    vertical: 13,
-                    horizontal: 12,
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 13,
+                  horizontal: 12,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: const BorderSide(
+                    width: 2,
+                    color: Colors.grey,
                   ),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: const BorderSide(
-                        width: 2,
-                        color: Colors.grey,
-                      )),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: const BorderSide(
-                        width: 2,
-                        color: Colors.blue,
-                      )),
-                  fillColor: Colors.white,
-                  filled: true,
-                  suffixIcon: const Icon(
-                    Icons.search,
-                    color: Colors.black,
+                ),
+                disabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: const BorderSide(
+                    width: 2,
+                    color: Colors.grey,
                   ),
-                  hintText: "Search Discount",
-                  hintStyle: GoogleFonts.rubik(fontSize: 12)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: const BorderSide(
+                    width: 2,
+                    color: Colors.blue,
+                  ),
+                ),
+                fillColor: Colors.white,
+                filled: true,
+                suffixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+                hintText: "Search Discount",
+                hintStyle: GoogleFonts.rubik(
+                  fontSize: 12,
+                ),
+              ),
             ),
           ),
           Expanded(
-            child: Container(
-              color: const Color(0xFFD1D1D1).withOpacity(0.5),
-              child: ListView(
-                children: [
-                  for (var gradient in gradients)
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 24, vertical: 10),
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        gradient: gradient,
-                      ),
-                      child: const Center(child: Text("Hello")),
+            child: ListView(
+              children: [
+                for (var gradient in gradients)
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 10,
                     ),
-                ],
-              ),
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: gradient,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 24),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Discount Opening - 50%",
+                            style: GoogleFonts.rubik(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            "Opening Discount for new customer.",
+                            style: GoogleFonts.rubik(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+              ],
             ),
           )
         ],
