5a24fedd9a135de5de36e911e849faf0be16f0ab
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-09-07 12:45:36 +0500

    some changes

--- lib/second_task_screen.dart
+++ lib/second_task_screen.dart
@@ -7,7 +7,7 @@ class SecondTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
           "Cashier",
@@ -28,71 +28,116 @@ class SecondTaskScreen extends StatelessWidget {
       body: Column(
         children: [
           const Divider(
-            thickness: 1,
             height: 1,
+            thickness: 0.5,
+            indent: 0,
+            endIndent: 0,
+            color: Color(0xFFD1D1D1),
           ),
           Container(
-            height: 56,
             color: Colors.white,
+            height: 56,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                const Expanded(
-                  child: Row(
-                    children: [
-                      SizedBox(width: 24),
-                      Text("All Products"),
-                      SizedBox(width: 8),
-                      Icon(Icons.expand_more_outlined),
-                    ],
-                  ),
+                const Text(
+                  "All products",
+                  style: TextStyle(fontSize: 16),
                 ),
-                Expanded(
-                    child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    const VerticalDivider(thickness: 1),
-                    SvgPicture.asset("assets/images/search.svg"),
-                    const VerticalDivider(thickness: 1),
-                    SvgPicture.asset("assets/images/bar_code.svg"),
-                    const VerticalDivider(thickness: 1),
-                    SvgPicture.asset("assets/images/list.svg"),
-                  ],
-                ))
+                const Icon(Icons.expand_more_rounded),
+                SizedBox(width: MediaQuery.of(context).size.width / 6),
+                const VerticalDivider(
+                  width: 0,
+                  thickness: 1,
+                  indent: 0,
+                  endIndent: 0,
+                  color: Color(0xFFD1D1D1),
+                ),
+                SvgPicture.asset("assets/images/search.svg"),
+                const VerticalDivider(
+                  width: 0,
+                  thickness: 1,
+                  indent: 0,
+                  endIndent: 0,
+                  color: Color(0xFFD1D1D1),
+                ),
+                SvgPicture.asset("assets/images/bar_code.svg"),
+                const VerticalDivider(
+                  width: 0,
+                  thickness: 1,
+                  indent: 0,
+                  endIndent: 0,
+                  color: Color(0xFFD1D1D1),
+                ),
+                SvgPicture.asset("assets/images/list.svg"),
               ],
             ),
           ),
           Expanded(
             child: GridView.count(
+              padding: const EdgeInsets.all(24),
               crossAxisCount: 2,
-              mainAxisSpacing: 20,
-              crossAxisSpacing: 20,
-              padding: const EdgeInsets.all(16),
+              mainAxisSpacing: 23,
+              crossAxisSpacing: 23,
+              childAspectRatio: 0.8,
               children: [
                 for (int i = 0; i < 100; i++)
                   Container(
                     decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(16)),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 12,
+                          spreadRadius: 5,
+                          color: Colors.grey.shade300,
+                          offset: const Offset(2, 2),
+                        )
+                      ],
+                    ),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         ClipRRect(
-                          borderRadius: BorderRadius.circular(16),
-                          child: Image.asset(
-                            "assets/images/meal.jpeg",
-                            width: MediaQuery.of(context).size.width / 2,
-                            fit: BoxFit.cover,
-                          ),
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.network(
+                              "https://www.eatingwell.com/thmb/m5xUzIOmhWSoXZnY-oZcO9SdArQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/article_291139_the-top-10-healthiest-foods-for-kids_-02-4b745e57928c4786a61b47d8ba920058.jpg"),
                         ),
+                        const SizedBox(height: 8),
                         const Padding(
-                          padding:
-                              EdgeInsets.symmetric(vertical: 5, horizontal: 12),
+                          padding: EdgeInsets.only(left: 12),
                           child: Text(
                             "Salad Tuna",
                             style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w600),
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14,
+                            ),
                           ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 12),
+                          child: Text(
+                            "Must choose level",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 12,
+                              color: const Color(0xFF2A3256).withOpacity(0.5),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            const Text(
+                              "\$10.99",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            SvgPicture.asset("assets/images/add.svg")
+                          ],
                         )
                       ],
                     ),
